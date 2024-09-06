394ad74476a13f9e3a11051c53d542b0a7156de0
9thCore <38700349+9thCore@users.noreply.github.com>
2024-09-06 21:40:11 +0300

    add failsafes to the post action array

--- .minecraft/kubejs/startup_scripts/schema/lychee/post/components.js
+++ .minecraft/kubejs/startup_scripts/schema/lychee/post/components.js
@@ -6,31 +6,48 @@ const LycheePostActions = {};
     let anyString = null; // Set lazily when getAny() is called
 
     /**
+     * @description Compiles the Post Action's IDs into a human-readable string
+     * @param {{id: string, post: Internal.RecipeComponent}[]} allPostActions
+     * @returns {string[]}
+     */
+    const listPossibleIDs = (allPostActions) => {
+        const IDs = [];
+        for (let action of allPostActions) {
+            IDs.push(`"${action.id}"`);
+        }
+        return IDs.join(" | ");
+    }
+
+    /**
      * 
      * @param {Internal.RecipeSchemaRegistryEventJS} id ID of the Post Action
      * @param {Internal.RecipeComponent} possibleValues All possible values of keys in the Post Action (presumably obtained through an "or" chain)
      * @param {Function} validator Validator run on every component of the Post Action: first arg is key, second arg is value, return value is [passed: boolean, errorMessage: string]
      * @param {Function} dataFixer Fixer for the entire object, used for things like converting {item: "2x minecraft:stone"} to {item: "minecraft:stone", count: 2}
+     * @returns {{id: string, post: Internal.RecipeComponent}}
      */
     const post = (id, possibleValues, validator, dataFixer) => {
-        return possibleValues.asMap(anyString).mapIn(object => {
-            if (object.type !== id) {
-                return null;
-            }
-            for (let key in object) {
-                // Skip over the "type" key
-                if (key === "type") {
-                    continue;
-                }
-                let [passed, error] = validator.call(null, key, object[key]);
-                if (!passed) {
-                    console.error(error);
+        return {
+            id: id,
+            post: possibleValues.asMap(anyString).mapIn(object => {
+                if (object.type !== id) {
                     return null;
                 }
-            }
-            dataFixer.call(null, object);
-            return object;
-        });
+                for (let key in object) {
+                    // Skip over the "type" key
+                    if (key === "type") {
+                        continue;
+                    }
+                    let [passed, error] = validator.call(null, key, object[key]);
+                    if (!passed) {
+                        console.error(error);
+                        return null;
+                    }
+                }
+                dataFixer.call(null, object);
+                return object;
+            })
+        };
     }
 
     /**
@@ -79,12 +96,24 @@ const LycheePostActions = {};
      */
     const getAny = Component => {
         anyString = Component("anyString");
-        const all = getAll(Component);
+        const allPostActions = getAll(Component);
 
-        let postAny = all[0];
-        for (let i = 1; i < all.length; i++) {
-            postAny = postAny.or(all[i]);
+        const noValidPostType = Component("anyString").mapIn(object => {
+            if (typeof object !== "object") {
+                console.SERVER.error(`A Post Action be an object!`);
+            } else if ("type" in object) {
+                console.SERVER.error(`${object.type} is not a valid Post Action type. Must be one of ${listPossibleIDs(allPostActions)}`);
+            } else {
+                console.SERVER.error(`A Post Action must have a type, one of ${listPossibleIDs(allPostActions)}`);
+            }
+            return null;
+        });
+
+        let postAny = allPostActions[0].post;
+        for (let i = 1; i < allPostActions.length; i++) {
+            postAny = postAny.or(allPostActions[i].post);
         }
+        postAny = postAny.or(noValidPostType);
 
         return postAny.asArrayOrSelf();
     };
