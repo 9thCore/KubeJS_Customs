d64efa93c294b3964d37218baa1db70c406faeb9
9thCore <38700349+9thCore@users.noreply.github.com>
2024-09-06 17:48:25 +0300

    block_interacting, no post actions or conditions yet

---
+++ .minecraft/kubejs/server_scripts/recipe/lychee.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.recipes.lychee.block_interacting(
+        "minecraft:iron_pickaxe",
+        "minecraft:stone"
+    );
+});
---
+++ .minecraft/kubejs/startup_scripts/schema/lychee.js
@@ -0,0 +1,44 @@
+// Carefully contain the radioactive matter and don't let any of this spill into the environment
+(function() {
+    const $RecipeSchema = Java.loadClass("dev.latvian.mods.kubejs.recipe.schema.RecipeSchema");
+    const commonProperties = []; // Set inside of the registry event
+
+    /**
+     * @description Add optional properties which are common to every recipe type
+     * @param {object[]} specificProperties Properties specific for this recipe only (i.e. not any shared properties)
+     */
+    const applyCommonProperties = specificProperties => {
+        return specificProperties.concat(commonProperties);
+    }
+
+    /**
+     * @description Add properties common to all recipe types and register the given schema
+     * @param {Internal.RecipeSchemaRegistryEventJS} event man this class name is long
+     * @param {ResourceLocation} id
+     * @param {object[]} specificProperties Properties specific for this recipe only (i.e. without any shared properties)
+     */
+    const register = (event, id, specificProperties) => {
+        event.register(id, new $RecipeSchema(applyCommonProperties(specificProperties)));
+    }
+
+    /**
+     * @description Get the Convenient Component Helper (TM)
+     * @param {Internal.RecipeSchemaRegistryEventJS} event 
+     * @returns 
+     */
+    const convenientComponentHelper = event => {
+        return (name, args) => event.components[name](args);
+    }
+
+    StartupEvents.recipeSchemaRegistry(event => {
+        const Component = convenientComponentHelper(event);
+        
+        const itemIn = Component("inputItem").asArrayOrSelf();
+        const itemInKey = itemIn.key("item_in");
+
+        const blockIn = Component("inputBlock").asArrayOrSelf();
+        const blockInKey = blockIn.key("block_in");
+
+        register(event, "lychee:block_interacting", [itemInKey, blockInKey]);
+    });
+})();
