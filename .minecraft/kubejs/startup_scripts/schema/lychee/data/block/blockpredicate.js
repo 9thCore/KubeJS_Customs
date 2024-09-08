284978523646ad9339a16e500aab2a7828dd0bf6
9thCore <38700349+9thCore@users.noreply.github.com>
2024-09-08 14:31:24 +0300

    add BlockPredicate

---
+++ .minecraft/kubejs/startup_scripts/schema/lychee/data/block/blockpredicate.js
@@ -0,0 +1,46 @@
+(function() {
+    /**
+     * @param {Function} Component The Convenient Component Helper (TM)
+     * @param {Function} Builder The Convenient Builder Helper (TM)
+     * @returns {Internal.RecipeComponent} Component that represents a BlockPredicate
+     */
+    const get = (Component, Builder) => {
+        const anyString = Component("anyString");
+
+        const predicate = Builder([
+            anyString.asArray().key("blocks").defaultOptional(),
+            anyString.key("tag").defaultOptional(),
+            LycheeSchemaFunctionality.NBTComponent.getOrString(Component).key("nbt").defaultOptional(),
+            LycheeSchemaFunctionality.Block.StatePropertiesPredicate.getKey(Component, Builder)
+        ]).mapIn(object => {
+            if (typeof object === "string") {
+                if (object.startsWith("#")) {
+                    return {
+                        tag: object.substring(1)
+                    };
+                }
+
+                return {
+                    blocks: [object]
+                };
+            }
+
+            return object;
+        }).mapOut(json => { // Because there can be the special case "*" instead of an object
+            if (json.has("blocks")) {
+                const array = json.get("blocks").getAsJsonArray();
+                if (array.size() >= 1 && array.get(0).getAsString() === "*") {
+                    return new LycheeSchemaFunctionality.LoadedClasses.$JsonPrimitive("*");
+                }
+            }
+
+            return json;
+        });
+        
+        return predicate;
+    }
+
+    StartupEvents.init(() => {
+        LycheeSchemaFunctionality.Block.BlockPredicate.get = get;
+    });
+})();
